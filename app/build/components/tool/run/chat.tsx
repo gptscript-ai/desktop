a4e7dcbb59c594a23272a2459457843432bd7ab9
tylerslaton <mtslaton1@gmail.com>
2024-05-22 16:23:01 -0400

    feat: add markdown rendering and fullscreen for chat window
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- app/build/components/tool/run/chat.tsx
+++ app/build/components/tool/run/chat.tsx
@@ -5,8 +5,12 @@ import { io, Socket } from "socket.io-client";
 import { IoCloseSharp } from "react-icons/io5";
 import { BuildContext } from "@/app/build/page";
 import { FaBackward } from "react-icons/fa";
+import { GrExpand, } from "react-icons/gr";
+import { BsArrowsFullscreen } from "react-icons/bs";
+import { HiOutlineArrowsPointingIn } from "react-icons/hi2";
 import { Run, type Frame, type Property } from "@gptscript-ai/gptscript";
-import { 
+import ReactMarkdown from "react-markdown";
+import {
     Card,
     CardHeader,
     CardBody,
@@ -14,6 +18,11 @@ import {
     Button,
     Divider,
     Input,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
 } from "@nextui-org/react";
 
 enum MessageType {
@@ -32,172 +41,281 @@ interface ChatBoxProps {
     params: Record<string, Property> | undefined;
 }
 
-export default function Chat({ name, file, params}: ChatBoxProps) {
+export default function Chat({ name, file, params }: ChatBoxProps) {
     const [messages, setMessages] = useState<Message[]>([]);
     const [connected, setConnected] = useState(false);
     const [socket, setSocket] = useState<Socket | null>(null);
-    const {setChatPanel, setRun } = useContext(BuildContext);
+    const { setChatPanel, setRun } = useContext(BuildContext);
     const [showForm, setShowForm] = useState(true);
     const [formValues, setFormValues] = useState<Record<string, string>>({});
+    const [showModal, setShowModal] = useState(false);
+    const [fullscreen, setFullscreen] = useState(false);
 
     useEffect(() => {
         const socket = io();
 
-        socket.on('connect', () => {
+        socket.on("connect", () => {
             setConnected(true);
             setRun(null);
-        })
-
-        socket.on('message', data => {
-            console.log('system message:', data);
         });
 
-        socket.on('scriptMessage', data => {
-            setMessages((prevMessages) => [...prevMessages, 
-                { type: MessageType.Bot, message: data }
+        socket.on("message", (data) => {
+            console.log("system message:", data);
+        });
+
+        socket.on("scriptMessage", (data) => {
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { type: MessageType.Bot, message: data },
             ]);
         });
 
-        socket.on('state', (data: Run) => setRun(data));
-        socket.on('disconnect', () => { setConnected(false) });
+        socket.on("state", (data: Run) => setRun(data));
+        socket.on("disconnect", () => {
+            setConnected(false);
+        });
 
         setSocket(socket);
 
-        return () => { 
+        return () => {
             setSocket(null);
-            socket.disconnect()
+            socket.disconnect();
         };
     }, []);
 
     useEffect(() => {
-        const messagesContainer = document.getElementById("message");
-        if (messagesContainer) {
-            messagesContainer.scrollTop = messagesContainer.scrollHeight;
+        const smallBody = document.getElementById("small-message");
+        if (smallBody) {
+            smallBody.scrollTop = smallBody.scrollHeight;
+        }
+        const largeBody = document.getElementById("large-message");
+        if (largeBody) {
+            largeBody.scrollTop = largeBody.scrollHeight;
         }
     }, [messages]);
-    
+
     const handleFormSubmit = () => {
         setShowForm(false);
         setMessages([]);
-        socket?.emit('run', file, name, formValues);
-    }
+        socket?.emit("run", file, name, formValues);
+    };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
         setFormValues((prevValues) => ({
             ...prevValues,
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
         }));
-    }
+    };
 
     const handleMessageSent = (message: Message) => {
         if (!socket || !connected) return;
         setMessages((prevMessages) => [...prevMessages, message]);
-        socket.emit('userMessage', message.message);
+        socket.emit("userMessage", message.message);
     };
 
-    return (
-        <Card className="h-full">
-             <CardHeader className="flex flex-col gap-1 py-2 px-4">
-                <div className="w-full flex justify-between">
-                    <h1 className={subtitle()}>
-                        { showForm ? "You're about to chat with " : "You're chatting with "}
-                        <span className="capitalize font-bold text-primary">{name}</span>
-                    </h1>
-                    <Button
-                        radius="full"
-                        isIconOnly
-                        color="primary"
-                        onPress={(_) => setChatPanel(<></>)}
-                    >
-                        <IoCloseSharp />
-                    </Button>
-                </div>
-            </CardHeader>
-            <Divider />
-            <CardBody id="message" className="shadow px-6 pt-6 overflow-y-scroll h-[300px]">
-                {showForm ? (
-                    <form>
-                        {Object.entries(params || {}).map(([argName, arg]) => (
-                            <Input
-                                className="mb-6"
-                                size="lg"
-                                label={argName}
-                                placeholder={arg.description}
-                                type="text"
-                                id={argName}
-                                name={argName}
-                                value={formValues[argName] || ''}
-                                onChange={handleInputChange}
-                            />
-                        ))}
-                    </form>
-                ) : (
-                    <div>
-                        {messages.map((message, index) => (
-                            message.type === MessageType.User ? (
-                                <div key={index} className="flex flex-col items-end mb-2">
-                                    <div className="rounded-2xl bg-blue-500 text-white py-2 px-4 max-w-[75%]">
-                                        {messages[index].message}
-                                    </div>
-                                </div>
-                            ) : (
-                                <div key={index} className="flex flex-col items-start mb-2">
-                                    <div className="rounded-2xl bg-gray-200 text-black dark:text-white dark:bg-zinc-700 py-2 px-4 max-w-[75%]">
-                                        {messages[index].message}
-                                    </div>
-                                </div>
-                            )
-                        ))}
+    const Messages = () => <div>
+        {messages.map((message, index) =>
+            message.type === MessageType.User ? (
+                <div
+                    key={index}
+                    className="flex flex-col items-end mb-2"
+                >
+                    <div className="rounded-2xl bg-blue-500 text-white py-2 px-4 max-w-full">
+                        {messages[index].message}
                     </div>
-                )}
-            </CardBody>
+                </div>
+            ) : (
+                <div
+                    key={index}
+                    className="flex flex-col items-start mb-2"
+                >
+                    <div className="rounded-2xl bg-gray-200 text-black dark:text-white dark:bg-zinc-700 py-2 px-4 max-w-full">
+                        <ReactMarkdown className="prose">
+                            {messages[index].message}
+                        </ReactMarkdown>
+                    </div>
+                </div>
+            )
+        )}
+    </div>
 
-            <CardFooter>
-                {showForm ? (
-                    <Button
-                        className="w-full"
-                        type="submit"
-                        color="primary"
-                        onPress={handleFormSubmit}  
+
+    const ChatBar = () => <>
+        <Button
+            startContent={<FaBackward />}
+            isIconOnly
+            radius="full"
+            className="mr-2 my-auto text-lg"
+            onPress={() => {
+                setRun(null);
+                setMessages([]);
+                setShowForm(true);
+            }}
+        />
+        <input
+            id="chatInput"
+            autoComplete="off"
+            className="border border-gray-300 dark:border-zinc-700 rounded-full shadow px-3 py-2 w-full focus:outline-primary"
+            placeholder="Ask the chat bot something..."
+            onKeyDown={(
+                event: React.KeyboardEvent<HTMLInputElement>
+            ) => {
+                if (event.key === "Enter") {
+                    handleMessageSent({
+                        type: MessageType.User,
+                        message: event.currentTarget.value,
+                    });
+                    event.currentTarget.value = "";
+                }
+            }}
+        />
+        <Button
+            startContent={<IoMdSend />}
+            isIconOnly
+            radius="full"
+            className="ml-2 my-auto text-lg"
+            color="primary"
+            onPress={() => {
+                const input = document.querySelector(
+                    "#chatInput"
+                ) as HTMLInputElement;
+                handleMessageSent({
+                    type: MessageType.User,
+                    message: input.value,
+                });
+                input.value = "";
+            }}
+        />
+    </>
+
+    return (
+        <>
+            <Card className="h-full" style={{ height: "100%" }}>
+                <CardHeader className="flex flex-col gap-1 py-2 px-4">
+                    <div className="w-full flex justify-between">
+                        <h1 className={subtitle()}>
+                            {showForm
+                                ? "You're about to chat with "
+                                : "You're chatting with "}
+                            <span className="capitalize font-bold text-primary">
+                                {name}
+                            </span>
+                        </h1>
+                        <Button
+                            radius="full"
+                            size="sm"
+                            isIconOnly
+                            color="primary"
+                            onPress={(_) => setChatPanel(<></>)}
+                        >
+                            <IoCloseSharp />
+                        </Button>
+                        <Button
+                            radius="full"
+                            size="sm"
+                            isIconOnly
+                            color="primary"
+                            className="ml-2"
+                            onPress={(_) => setShowModal(true)}
+                        >
+                            <GrExpand />
+                        </Button>
+                    </div>
+                </CardHeader>
+                <Divider />
+                <CardBody
+                    id="small-message"
+                    className="shadow px-6 pt-6 overflow-y-scroll h-[300px]"
+                >
+                    {showForm ? (
+                        <form>
+                            {Object.entries(params || {}).map(
+                                ([argName, arg]) => (
+                                    <Input
+                                        className="mb-6"
+                                        size="lg"
+                                        label={argName}
+                                        placeholder={arg.description}
+                                        type="text"
+                                        id={argName}
+                                        name={argName}
+                                        value={formValues[argName] || ""}
+                                        onChange={handleInputChange}
+                                    />
+                                )
+                            )}
+                        </form>
+                    ) : (
+                        < Messages />
+                    )}
+                </CardBody>
+
+                <CardFooter>
+                    {showForm ? (
+                        <Button
+                            className="w-full"
+                            type="submit"
+                            color="primary"
+                            onPress={handleFormSubmit}
+                        >
+                            Start Chat
+                        </Button>
+                    ) : (
+                        <ChatBar />
+                    )}
+                </CardFooter>
+            </Card>
+            <Modal
+                isOpen={showModal}
+                onOpenChange={setShowModal}
+                hideCloseButton={true}
+                size={fullscreen ? "full" : "3xl"}
+                className={fullscreen ? "" : "h-4/5"}
+                
+            >
+                <ModalContent>
+                    <ModalHeader>
+                        <h1 className={subtitle()}>
+                            {showForm
+                                ? "You're about to chat with "
+                                : "You're chatting with "}
+                            <span className="capitalize font-bold text-primary">
+                                {name}
+                            </span>
+                        </h1>
+                        <Button
+                            radius="full"
+                            size="sm"
+                            isIconOnly
+                            color="primary"
+                            onPress={(_) => setShowModal(false)}
+                        >
+                            <IoCloseSharp />
+                        </Button>
+                        <Button
+                            radius="full"
+                            size="sm"
+                            isIconOnly
+                            color="primary"
+                            className="ml-2"
+                            onPress={(_) => setFullscreen(!fullscreen)}
+                        >
+                            { fullscreen ? <HiOutlineArrowsPointingIn className="text-lg"/> : <BsArrowsFullscreen />}
+                        </Button>
+                    </ModalHeader>
+                    <ModalBody
+                        id="large-message"
+                        className="shadow px-6 pt-6 overflow-y-scroll"
                     >
-                        Start Chat
-                    </Button>
-                ) : (<>
-                    <Button
-                        startContent={<FaBackward/>}
-                        isIconOnly radius="full"
-                        className="mr-2 my-auto text-lg"
-                        onPress={() => {
-                            setRun(null)
-                            setMessages([])
-                            setShowForm(true)
-                        }}
-                    />
-                    <input
-                        id="chatInput"
-                        autoComplete="off"
-                        className="border border-gray-300 dark:border-zinc-700 rounded-full shadow px-3 py-2 w-full focus:outline-primary"
-                        placeholder="Ask the chat bot something..."
-                        onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                            if (event.key === "Enter") {
-                                handleMessageSent({ type: MessageType.User, message: event.currentTarget.value });
-                                event.currentTarget.value = "";
-                            }
-                        }}
-                    />
-                    <Button
-                        startContent={<IoMdSend/>}
-                        isIconOnly radius="full"
-                        className="ml-2 my-auto text-lg"
-                        color="primary"
-                        onPress={() => {
-                            const input = document.querySelector("#chatInput") as HTMLInputElement;
-                            handleMessageSent({ type: MessageType.User, message: input.value });
-                            input.value = "";
-                        }}
-                    />
-                </>)}
-            </CardFooter>
-        </Card>
+                        <Messages />
+                    </ModalBody>
+                    <ModalFooter>
+                        <ChatBar />
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,8 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui(),
+    require('@tailwindcss/typography'),
+  ],
 }
