c9d4977563042e2a3d9254a16dc582f81eb47b32
tylerslaton <mtslaton1@gmail.com>
2024-05-24 10:19:44 -0400

    feat: add docs link, update branding, and add loading to homepage
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>
-					GPTScript Builder
+				<h1 className={title() + ' drop-shadow-xl'}>
+					Script Builder
 				</h1>
 				<h2 className={subtitle({ class: "mt-4" })}>
 					Select a script below to run or edit. You can also create a new script from scratch.
--- app/run/layout.tsx
+++ app/run/layout.tsx
@@ -1,4 +1,4 @@
-export default function BlogLayout({
+export default function RunLayout({
 	children,
 }: {
 	children: React.ReactNode;
