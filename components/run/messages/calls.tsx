8b5734470583a9674c86831a0fc9a987a4934006
tylerslaton <mtslaton1@gmail.com>
2024-05-29 17:29:45 -0400

    feat: add expanding for stack trace button in chat ui
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- components/run/messages/calls.tsx
+++ components/run/messages/calls.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { GoNote } from "react-icons/go";
 import StackTrace from './calls/stackTrace';
 import type { CallFrame } from '@gptscript-ai/gptscript';
+import { IoCloseSharp } from 'react-icons/io5';
+import { BsArrowsFullscreen } from 'react-icons/bs';
+import { HiOutlineArrowsPointingIn } from 'react-icons/hi2';
 import {
     Modal,
     ModalContent,
@@ -12,13 +15,14 @@ import {
 } from "@nextui-org/react";
 
 const Calls = ({calls}: {calls: CallFrame[]}) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [fullscreen, setFullscreen] = useState(false);
 
     return (
         <div>
             <Tooltip content="View stack trace" closeDelay={0.5}>
                 <Button 
-                    onPress={() => setIsModalOpen(true)}
+                    onPress={() => setShowModal(true)}
                     isIconOnly
                     radius='full'
                 >
@@ -26,17 +30,38 @@ const Calls = ({calls}: {calls: CallFrame[]}) => {
                 </Button>
             </Tooltip>
             <Modal
-                isOpen={isModalOpen}
-                onOpenChange={setIsModalOpen}
-                size="4xl"
-                className="h-5/6"
-                backdrop='blur'
+                isOpen={showModal}
+                onOpenChange={setShowModal}
+                hideCloseButton={true}
+                size={fullscreen ? "full" : "3xl"}
+                className={fullscreen ? "" : "h-4/5"}
             >
                 <ModalContent>
-                    <ModalHeader>
+                    <ModalHeader className="flex justify-between">
                         <div>
                             <h1 className="text-2xl my-4">Stack Trace</h1>
                             <h2 className="text-base text-zinc-500">Below you can see what this call is doing or has done.</h2>
+                        </div>
+                        <div>
+                            <Button
+                                radius="full"
+                                size="sm"
+                                isIconOnly
+                                color="primary"
+                                onPress={(_) => setShowModal(false)}
+                            >
+                                <IoCloseSharp />
+                            </Button>
+                            <Button
+                                radius="full"
+                                size="sm"
+                                isIconOnly
+                                color="primary"
+                                className="ml-2"
+                                onPress={(_) => setFullscreen(!fullscreen)}
+                            >
+                                { fullscreen ? <HiOutlineArrowsPointingIn className="text-lg"/> : <BsArrowsFullscreen />}
+                            </Button>
                         </div>
                     </ModalHeader>
                     <ModalBody className='mb-4 h-full overflow-y-scroll'>
