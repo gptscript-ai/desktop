08daf518ccf96f0c4a299d280264a456bd33fea4
tylerslaton <mtslaton1@gmail.com>
2024-06-03 12:28:56 -0400

    feat: remove builder UI options when builder UI is off
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -12,7 +12,7 @@ import {
     Button,
 } from "@nextui-org/react";
 
-export default function Scripts() {
+export default function Scripts({buildOptions}: {buildOptions?: boolean}) {
     const [files, setFiles] = useState<Record<string,string>>({});
     const [loading, setLoading] = useState(true);
 
@@ -41,27 +41,34 @@ export default function Scripts() {
 
     const ScriptItems = () => files && Object.keys(files) ? (
         <div className="grid grid-cols-2 gap-6">
-            <Button
-                size="lg"
-                startContent={<VscNewFile/>}
-                color="primary"
-                className="col-span-2"
-                onPress={() => {{ window.location.href = '/build?file=new'}}}
-            >
-                Create a new script
-            </Button>
+            { buildOptions &&
+                <Button
+                    size="lg"
+                    startContent={<VscNewFile/>}
+                    color="primary"
+                    className="col-span-2"
+                    onPress={() => {{ window.location.href = '/build?file=new'}}}
+                >
+                    Create a new script
+                </Button>
+            }
             {Object.keys(files).map((file) => (
                 <Card key={file.replace(".gpt", "")} className="p-4">
-                    <CardHeader className="flex justify-between ">
+                    <CardHeader className="flex justify-between">
                         <div className="flex gap-3 items-center">
                             <FaRegFileCode />
                             {file.replace('.gpt', '')}
                         </div>
-                        <div className="flex-col flex absolute bottom-1 right-4">
-                            <Button startContent={<GoPaperAirplane />} onPress={() => {{ window.location.href = `/run?file=${file.replace('.gpt', '')}`;}}} radius="full" variant="light" isIconOnly/>
-                            <Button startContent={<GoPencil />} onPress={() => {{ window.location.href = `/build?file=${file.replace('.gpt', '')}`;}}} radius="full" variant="light" isIconOnly/>
-                            <Button startContent={<GoTrash />} onPress={() => {deleteFile(file.replace('.gpt', ''))}}radius="full" variant="light" isIconOnly/>
-                        </div>
+                        { buildOptions ? 
+                            <div className="flex-col flex absolute bottom-1 right-4">
+                                <Button startContent={<GoPaperAirplane />} onPress={() => {{ window.location.href = `/run?file=${file.replace('.gpt', '')}`;}}} radius="full" variant="light" isIconOnly/>
+                                <Button startContent={<GoPencil />} onPress={() => {{ window.location.href = `/build?file=${file.replace('.gpt', '')}`;}}} radius="full" variant="light" isIconOnly/>
+                                <Button startContent={<GoTrash />} onPress={() => {deleteFile(file.replace('.gpt', ''))}}radius="full" variant="light" isIconOnly/>
+                            </div> :
+                            <div className="absolute right-4 top-10">
+                                <Button startContent={<GoPaperAirplane />} onPress={() => {{ window.location.href = `/run?file=${file.replace('.gpt', '')}`;}}} radius="full" variant="light" size="lg" isIconOnly/>    
+                            </div>
+                        }
                     </CardHeader>
                     <CardBody>
                         <p className="truncate w-4/5 text-zinc-500">{files[file] ? files[file] : "No description provided"}</p>
