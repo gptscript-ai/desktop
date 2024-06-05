1311f6838601c11ec7ab8b29afc4a7e42f68cd05
tylerslaton <mtslaton1@gmail.com>
2024-06-05 13:34:41 -0400

    style: update messaging on homepage
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
 				<h1 className={title() + ' drop-shadow-xl'}>
-					Script Builder
+					My Scripts
 				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Select a script below to run or edit. You can also create a new script from scratch.
+				<h2 className={subtitle({ class: "mt-4 mb-10" })}>
+					Select a script below to run.
 				</h2>
 			</div>
 
