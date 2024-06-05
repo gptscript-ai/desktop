bb3ac55fd2e811e0ad6c08f6911288558f1b3557
tylerslaton <mtslaton1@gmail.com>
2024-06-05 13:34:41 -0400

    style: update messaging on homepage
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
 		<NextUINavbar maxWidth="xl" position="sticky">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1" href="">
+					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
 						<p className="font-bold text-inherit text-xl">GPTScript</p>
 					</NextLink>
