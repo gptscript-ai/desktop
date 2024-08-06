f98d9c927392ac867b8320a623d07215768206ab
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-08-06 19:46:48 -0400

    fix: set workspace dir from correct env var
    And store threads and workspaces in the data dir instead of the cache
    dir.
    
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- config/env.ts
+++ config/env.ts
@@ -2,12 +2,11 @@ import {GPTScript} from "@gptscript-ai/gptscript"
 import path from "path";
 
 export const SCRIPTS_PATH = () => process.env.SCRIPTS_PATH || "gptscripts";
-export const WORKSPACE_DIR = () => process.env.GPTSCRIPT_WORKSPACE_DIR || "";
+export const WORKSPACE_DIR = () => process.env.WORKSPACE_DIR || "";
 export const THREADS_DIR = () => process.env.THREADS_DIR || path.join(WORKSPACE_DIR(), "threads");
 export const GATEWAY_URL = () => process.env.GATEWAY_URL || "http://localhost:8080";
 
 export const set_WORKSPACE_DIR = (dir: string) => process.env.GPTSCRIPT_WORKSPACE_DIR = dir;
-export const set_SCRIPTS_PATH = (dir: string) => process.env.SCRIPTS_PATH = dir;
 
 let gptscript: GPTScript | null = null;
 
