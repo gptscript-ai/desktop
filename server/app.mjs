878f61e992f4bac2c02dc1890863b806c4c6c65e
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-08-08 00:36:19 -0400

    fix: add metadata to gateway creds tool to pull tool runtimes
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- server/app.mjs
+++ server/app.mjs
@@ -447,7 +447,7 @@ function gptscriptConfigPath() {
     if (os.platform() === 'darwin') {
         configDir = process.env.XDG_CONFIG_HOME || path.join(homeDir, 'Library', 'Application Support')
     } else if (os.platform() === 'win32') {
-        configDir = process.env.APPDATA || path.join(homeDir, 'AppData', 'Roaming');
+        configDir = path.join(homeDir, 'AppData', 'Local');
     } else if (os.platform() === 'linux') {
         configDir = process.env.XDG_CONFIG_HOME || path.join(homeDir, '.config');
     } else {
