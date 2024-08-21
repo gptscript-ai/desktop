2682446ef7f07cf41bb2c4781e66a5c016dbc464
Grant Linville <grant@acorn.io>
2024-08-21 10:41:28 -0400

    fix: remove credential tools from dynamically added tools (#211)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- server/app.mjs
+++ server/app.mjs
@@ -253,7 +253,35 @@ const mount = async (
     socket.emit('addingTool');
 
     const loaded = await gptscript.loadTools(script, true);
-    const newTools = toChatStateTools(loaded?.program?.toolSet);
+
+    // We need to filter out credential tools so that we do not give them to the LLM.
+    // TODO - also filter out context tools maybe?
+    const toolSet = {};
+    const credentials = new Set();
+    for (tool of Object.values(loaded?.program?.toolSet)) {
+      if (tool.credentials) {
+        for (let cred of tool.credentials) {
+          for (let mapping of tool.toolMapping[cred]) {
+            credentials.add(mapping.toolID);
+          }
+        }
+      }
+      if (tool['exportCredentials']) {
+        for (let cred of tool['exportCredentials']) {
+          for (let mapping of tool.toolMapping[cred]) {
+            credentials.add(mapping.toolID);
+          }
+        }
+      }
+    }
+
+    for (let [key, value] of Object.entries(loaded?.program?.toolSet)) {
+      if (!credentials.has(key)) {
+        toolSet[key] = value;
+      }
+    }
+
+    const newTools = toChatStateTools(toolSet);
     const currentState = JSON.parse(state.chatState);
     currentState.continuation.state.completion.tools = newTools;
 
